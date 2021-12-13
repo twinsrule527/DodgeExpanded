e1e7b6799e6821f40e0f7db45247fddc94e9d398
Eli <eliclendenning@gmail.com>
2021-12-12 20:37:30 -0500

    Added transition time - game loops

--- Assets/EliWork/Scripts/FinalCheckpoint.cs
+++ Assets/EliWork/Scripts/FinalCheckpoint.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class FinalCheckpoint : MonoBehaviour
 {
     [SerializeField] private Image FadeToBlack;
@@ -9,6 +10,7 @@ public class FinalCheckpoint : MonoBehaviour
     [SerializeField] private AudioSource backgroundSound;
     private float baseSoundLevel;
     private float curTime;
+    bool disabled;
     void Start()
     {
         baseSoundLevel = backgroundSound.volume;
@@ -16,12 +18,20 @@ public class FinalCheckpoint : MonoBehaviour
 
     void Update()
     {
-        curTime += Time.deltaTime;
-        curTime = Mathf.Clamp(curTime, 0, timeToLerp);
-        FadeToBlack.color = Color.Lerp(new Color(0, 0, 0, 0), Color.black, curTime / timeToLerp);
-        backgroundSound.volume = Mathf.Lerp(baseSoundLevel, 0, curTime / timeToLerp);
-        if(timeToLerp == curTime) {
-            gameObject.SetActive(false);
+        if(!disabled) {
+            curTime += Time.deltaTime;
+            curTime = Mathf.Clamp(curTime, 0, timeToLerp);
+            FadeToBlack.color = Color.Lerp(new Color(0, 0, 0, 0), Color.black, curTime / timeToLerp);
+            backgroundSound.volume = Mathf.Lerp(baseSoundLevel, 0, curTime / timeToLerp);
+            if(timeToLerp == curTime) {
+                disabled = true;
+                StartCoroutine("Restart");
+            }
         }
+    }
+
+    private IEnumerator Restart() {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(0);
     }
 }
